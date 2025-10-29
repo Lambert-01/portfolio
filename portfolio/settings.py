405df3e5f83de8ff99fc7fa2b3f82745b82940e0
Lambert-01 <159195561+Lambert-01@users.noreply.github.com>
2025-10-29 18:30:23 +0200

    Configure Django for production deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -8,12 +8,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xjt3yo^&8fcmaq=x))1ki5kavu)2eno4^2le1!_fo7p0jps-*!'
+import os
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-xjt3yo^&8fcmaq=x))1ki5kavu)2eno4^2le1!_fo7p0jps-*!')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['portfolio-pa3d.onrender.com', 'localhost', '127.0.0.1']  # Update with your domain name(s) or IP address for production
+ALLOWED_HOSTS = ['portfolio-pa3d.onrender.com', 'localhost', '127.0.0.1', '0.0.0.0', 'portfolio.onrender.com']  # Update with your domain name(s) or IP address for production
 
 # Application definition
 INSTALLED_APPS = [
